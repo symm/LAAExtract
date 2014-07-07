3aaf2cc13370bb382ff48983b43eb2f5368fe480
Gareth Jones <me@gazj.co.uk>
2014-07-07 21:05:13 +0100

    Extract Loom audio to method, add better progress output

--- extract.py
+++ extract.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding: utf-8
 # Helps you to run the Steam LucasArts Adventure Pack in ScummVM
 # Licence: GPL
 
@@ -119,7 +120,8 @@ def extract_game_data(extract_information, steam_path, destination_directory):
                                 extract_information['source_dir'],
                                 extract_information['source_binary'])
     if not os.path.isfile(game_binary):
-        print "ERROR: Unable to extract %s because (%s) is missing" % (extract_information['name'], game_binary)
+        print ("\033[91m✘\033[0m %s Does not appear to be installed"
+               % (extract_information['name']))
         return False
 
     f = open(game_binary, "rb")
@@ -129,19 +131,25 @@ def extract_game_data(extract_information, steam_path, destination_directory):
     f.close()
 
     if "%x" % extract_checksum != extract_information['checksum']:
-        print ("Unable to extract %s from %s"
-               % (extract_information['destination_file'], game_binary))
-        print ("(checksum %x didn't match %s)"
-               % (extract_checksum, extract_information['checksum']))
+        print (
+            "\033[91m✘\033[0m Failed to extract %s (%s) - checksum mismatch" %
+            (extract_information['name'], extract_information['destination_file']))
         return False
 
-    if not os.path.exists(destination_directory + '/' + extract_information['destination_dir']):
-        os.mkdir(destination_directory + '/' + extract_information['destination_dir'])
+    if not os.path.exists(
+            destination_directory +
+            '/' +
+            extract_information['destination_dir']):
+        os.mkdir(
+            destination_directory +
+            '/' +
+            extract_information['destination_dir'])
     target = destination_directory + '/' + \
         extract_information['destination_dir'] + '/' + extract_information['destination_file']
     r = open(target, "wb")
 
     if extract_information['name'] == 'Loom Audio':
+        print 'Extracting Loom audio, process takes several minutes...'
         r.write(struct.pack('BBBB', 0x52, 0x49, 0x46, 0x46))
         r.write(struct.pack('I', 579123588))
         r.write(struct.pack('BBBB', 0x57, 0x41, 0x56, 0x45))
@@ -155,50 +163,62 @@ def extract_game_data(extract_information, steam_path, destination_directory):
         r.write(struct.pack('BB', 16, 0))
         r.write(struct.pack('BBBB', 0x64, 0x61, 0x74, 0x61))
         r.write(struct.pack('I', 579123552))
-        frame = 0
-        new_extract = ""
-        print 'Extracting Loom audio, process takes several minutes...'
-        data = enumerate(extract)
-        for byte in data:
-            if not (byte[0] % 1177):
-                frame += 1
-                bits = (ord(b) for b in byte[1])
-                for b in bits:
-                    lshift = b >> 4
-                bits = (ord(b) for b in byte[1])
-                for b in bits:
-                    rshift = b & 0x0F
-            else:
-                if (byte[0] %
-                    2 == 0 and frame %
-                    2 == 0) or (byte[0] %
-                                2 == 1 and frame %
-                                2 == 1):
-                    shift_by = lshift
-                else:
-                    shift_by = rshift
-                new_byte = struct.unpack('b', byte[1])[0]
-                new_byte = new_byte << shift_by
-                if(new_byte & 0x8):
-                    new_byte = -0x0 + new_byte
-                new_extract += struct.pack('<h', new_byte)
-            if frame % 2000 == 0:
-                sys.stdout.write(
-                    "%s %s %s\r" %
-                    ("Frame", frame, "of 246227."))
-                sys.stdout.flush()
-        sys.stdout.write("\n")
-        sys.stdout.flush()
-        extract = new_extract
-        extract_information['checksum'] = 'e9dee869'
+        extract = extract_loom_audio(extract)
+        # TODO: check datafile. should have crc32 e9dee869
 
     r.write(extract)
     r.close()
-    print ("Found and wrote to disk: %s (%s)"
-           % (target, extract_information['checksum']))
+    print ("\033[92m✔\033[0m Extracted %s"
+           % (extract_information['name']))
+
+
+def extract_loom_audio(extract):
+    frame = 0
+    new_extract = ""
+    data = enumerate(extract)
+
+    for byte in data:
+        if not (byte[0] % 1177):
+            frame += 1
+            bits = (ord(b) for b in byte[1])
+            for b in bits:
+                lshift = b >> 4
+            bits = (ord(b) for b in byte[1])
+            for b in bits:
+                rshift = b & 0x0F
+        else:
+            if (byte[0] %
+                2 == 0 and frame %
+                2 == 0) or (byte[0] %
+                            2 == 1 and frame %
+                            2 == 1):
+                shift_by = lshift
+            else:
+                shift_by = rshift
+            new_byte = struct.unpack('b', byte[1])[0]
+            new_byte = new_byte << shift_by
+            if(new_byte & 0x8):
+                new_byte = -0x0 + new_byte
+            new_extract += struct.pack('<h', new_byte)
+        if frame % 2000 == 0:
+            sys.stdout.write(
+                "%s %s %s\r" %
+                ("Frame", frame, "of 246227."))
+            sys.stdout.flush()
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+
+    return new_extract
+
+
+def get_banner():
+    return """                   ___     ___  __        __  ___
+|     /\   /\     |__  \_/  |  |__)  /\  /  `  |
+|___ /~~\ /~~\    |___ / \  |  |  \ /~~\ \__,  |
+        """
 
 if __name__ == "__main__":
-    print "LAAExtract v0.3"
+    print get_banner()
 
     if sys.platform == "darwin" or sys.platform.startswith("linux"):
         home_dir = os.path.expanduser("~")
@@ -221,7 +241,7 @@ if __name__ == "__main__":
         destination = home_dir + "/LAAextract"
         if not os.path.exists(destination):
             os.mkdir(destination)
-        print "Your loot will be dropped in " + destination + ' :)'
+        print "The game files will be extracted to: " + destination + '\n'
 
         if sys.platform == "darwin":
             for game in mac:
@@ -229,6 +249,8 @@ if __name__ == "__main__":
         else:
             for game in pc:
                 extract_game_data(pc[game], source, destination)
+
+        print "\nDon't forget to convert the Loom Track1.wav to flac, ogg, or mp3!"
 
     elif sys.platform == "win32" or sys.platform == "cygwin":
         import win32gui
@@ -252,5 +274,3 @@ if __name__ == "__main__":
                 shell.SHGetPathFromIDList(pidl),
                 shell.SHGetPathFromIDList(pidl))
         raw_input("Press Enter to continue...")
-
-    print "Don't forget to convert the Track1.wav to flac, ogg, or mp3!"
